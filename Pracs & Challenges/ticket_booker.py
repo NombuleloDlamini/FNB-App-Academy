d4813c20a90d2e93696d0deafbed64248ad09050
Nombulelo Dlamini <nombulelo.dlamini008@gmail.com>
2026-07-17 23:33:09 +0200

    Create ticket_booker.py

---
+++ Pracs & Challenges/ticket_booker.py
@@ -0,0 +1,5 @@
+## 
+
+name = input("Enter your name: ")
+artist = input("Enter the name of the band/artist you want to see: ")
+print(f"Hey {name}! Your tickets to see {artist} are booked successfully!")
